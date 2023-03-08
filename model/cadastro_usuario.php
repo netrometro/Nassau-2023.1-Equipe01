86d883c0bbb738a8e09ae7c71934ffe6dacd05c3
Valdenix07 <valdeniofilho2003@yahoo.com>
2023-03-08 19:12:55 -0300

    Tela atualizada e tentativa de conexao com banco

--- model/cadastro_usuario.php
+++ model/cadastro_usuario.php
@@ -2,11 +2,26 @@
 
     require('Config.php');
 
-    $name = FILTER_INPUT(INPUT_POST,'name');
+    $name = FILTER_INPUT(INPUT_POST,'nome');
     $email = FILTER_INPUT(INPUT_POST,'email');
     $cpf = FILTER_INPUT(INPUT_POST,'cpf');
     $telefone = FILTER_INPUT(INPUT_POST,'telefone');
     $endereco = FILTER_INPUT(INPUT_POST,'endereço');
-    $password = FILTER_INPUT(INPUT_POST,'password');
+    $password = FILTER_INPUT(INPUT_POST,'senha');
 
+  /*  if(isset($_POST['submit']))
+{
+include_once('config.php')
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['telefone'];
+$endereco = $_POST['endereco'];
+$password = $_POST['password'];
+
+$result = mysql_query($conn, "INSERT INTO (nome da tabela do BD)(nome,email,cpf,telefone,endereco,password) 
+VALUES ('$name','$email','$cpf','$telefone','$endereco','$password')"); 
+}
+*/
 ?>

08f3ab9cae6da30bc39df5e5c2f4386237dd8860
rafael2n2 <rafael19.2003@gmail.com>
2023-03-10 02:28:56 -0300

    Ajuste na localização de alguns arquivos, pequena mudança no head do index e Main finalizado

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=,, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <title>Login</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/img/Logo.png">
+    <title>CashLock</title>
 </head>
 <body>
     <div class="containerLogin">

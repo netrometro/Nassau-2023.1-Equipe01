56347b28a015191ccd8878f44ed7ad32dc7b450c
rafael2n2 <rafael19.2003@gmail.com>
2023-03-10 19:51:42 -0300

    Conserto do Bug do main

--- paginas/main.php
+++ paginas/main.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+
+    if( isset($_SESSION['token'])){
+        $token = $_SESSION['token'];
+        if(md5($_SESSION['userName']) <> $token){
+            header('Location: ../');
+        }
+    } else {
+        header('Location: ../');
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>

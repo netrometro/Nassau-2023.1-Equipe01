533c967a051ae52f484e36e3c555fdb8ffff2ea3
JONASPAULINO <jonaspaulinodasilva@gmail.com>
2023-03-12 11:55:51 -0300

    stilo do arquivo contas a receber

--- paginas/consulta_receber.php
+++ paginas/consulta_receber.php
@@ -14,42 +14,51 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <title>Consulta títulos a receber</title>
 </head>
-<body>
+<body> 
     <div class="containerReceber"> 
-        <header>
-            <button>Novo título</button>
-        </header>
-        <div class="consulta">
-            <table border="1" width="100%">
-                <tr>
-                    <th>ID</th>
-                    <th>NOME</th>
-                    <th>VALOR</th>
-                    <th>PAGO</th>
-                    <th>SALDO</th>
-                    <th>OPÇÕES</th>
-                </tr>
-                <?php foreach($titulosReceber as $receber): ?>
+        <div class="containerMenu">
+
+            <a class="btn" href="">Clientes</a>
+            <a class="btn" href="">Fornecedores</a>
+            <a class="btn" href="./consulta_receber.php">Receber</a>
+            <a class="btn" href="">Pagar</a>
+
+        </div> 
+        <div class="ContainerConsulta">
+            <header>
+                <button>Novo título</button>
+            </header>
+            <div class="consulta">
+                <table border="1" width="100%">
                     <tr>
-                        <td><?=$receber['id']?></td>
-                        <td><?=$receber['id_cliente']?></td>
-                        <td><?=$receber['valor']?></td>
-                        <td><?=$receber['pago']?></td>
-                        <td><?=$receber['saldo']?></td>
-                        <td>
-                            <a href="../model/processa_consulta_receber.php?editar=sim&id=<?=$receber['id']?>">
-                                <img width="20" src="../assets/img/edt.png" alt="botão editar registro.">
-                            </a>
-                            <a  href="../model/processa_consulta_receber.php?excluir=sim&id=<?=$receber['id']?>" 
-                                onclick="return confirm('Tem certeza que deseja excluir este registro?')">
-                                <img width="20" src="../assets/img/del.png" alt="botão excluir registro.">
-                            </a>    
-                        </td>
+                        <th id="tdId">ID</th>
+                        <th>NOME</th>
+                        <th>VALOR</th>
+                        <th>PAGO</th>
+                        <th>SALDO</th>
+                        <th id="tdOpcoes">OPÇÕES</th>
                     </tr>
-                <?php endforeach;?>
-            </table>
+                    <?php foreach($titulosReceber as $receber): ?>
+                        <tr>
+                            <td><?=$receber['id']?></td>
+                            <td><?=$receber['id_cliente']?></td>
+                            <td><?=$receber['valor']?></td>
+                            <td><?=$receber['pago']?></td>
+                            <td><?=$receber['saldo']?></td>
+                            <td>
+                                <a href="../model/processa_consulta_receber.php?editar=sim&id=<?=$receber['id']?>">
+                                    <img width="20" src="../assets/img/edt.png" alt="botão editar registro.">
+                                </a>
+                                <a  href="../model/processa_consulta_receber.php?excluir=sim&id=<?=$receber['id']?>" 
+                                    onclick="return confirm('Tem certeza que deseja excluir este registro?')">
+                                    <img width="20" src="../assets/img/del.png" alt="botão excluir registro.">
+                                </a>    
+                            </td>
+                        </tr>
+                    <?php endforeach;?>
+                </table>
+            </div>
         </div>
-    </div>
-    
+    </div>  
 </body>
 </html>

1d545d8616d50221d13236c44e7d5eb8174b04a1
rafael2n2 <rafael19.2003@gmail.com>
2023-03-17 21:26:15 -0300

    Pequenas atualizções no css do cadastro de Fornecedores

--- paginas/Fornecedor/fornecedor_cadastro.php
+++ paginas/Fornecedor/fornecedor_cadastro.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 
-    <a href="main.php">
+    <a href="fornecedor.php">
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="white" class="bi bi-arrow-left" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
         </svg>
@@ -21,8 +21,8 @@
     <section class="container">
 
         <div id="formulario">
-            <h1>Cadastrar Fornecedor</h1>
-            <form action="../../model/Fornecedor/fornecedor_cadastro.php" method="POST" >
+            <h1 class = "titulo-formulario">Cadastrar Fornecedor</h1>
+            <form id="formulario" action="../../model/Fornecedor/fornecedor_cadastro.php" method="POST" >
                 <div class="input-container">
                     <input type="text" id="nome" name="nome" class="text-input" placeholder="Nome" autocomplete="off" required/>
                     <label for="nome" class="label">Nome</label>

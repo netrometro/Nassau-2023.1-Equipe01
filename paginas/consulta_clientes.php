1bb16043cecba9741f631da23b3b4baa331a2e03
Valdenix07 <valdeniofilho2003@yahoo.com>
2023-03-24 20:29:26 -0300

    estilizacao adicionada na consulta de clientes

--- paginas/consulta_clientes.php
+++ paginas/consulta_clientes.php
@@ -23,28 +23,46 @@ $conn = null;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Consulta clientes</title>
 </head>
 <body>
-    <table border="1">
-        <tr>
-            <th>nome</th>
-            <th>endereço</th>
-            <th>telefone</th>
-            <th>ações</th>
-        </tr>
-        <?php foreach(new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v){
-            echo "<tr>";
-            echo "<td>".$v["nome"]."</td>";
-            echo "<td>".$v["endereco"]."</td>";
-            echo "<td>".$v["telefone"]."</td>";
-           echo "<td>
-           <a href='../paginas/edit_usuario.php?id=$v[id]' title='Editar'><img src='../assets/img/edt.png'\></a>
-            <a href='../model/clientesdelete.php?id=$v[id]' title='Deletar'><img src='../assets/img/del.png'\></a>
-            </td>";
-            echo "<tr>";
-        }
-        ?>
-    </table>
+  <div class="containerReceber"> 
+        <div class="containerMenu">
+            <a class="btn" href="./main.php">Início</a>
+            <a class="btn" href="">Clientes</a>
+            <a class="btn" href="./fornecedor.php">Fornecedores</a>
+            <a class="btn" href="./consulta_receber.php">Receber</a>
+            <a class="btn" href="">Pagar</a>
+        </div> 
+        <div class="ContainerConsulta">
+            <header>
+                  <a href="./clientesadd.php" title="Adicionar">Novo cliente</a>
+            </header>
+            <div class="consulta">
+                  <table border="1" width="100%">
+
+                    <tr>
+                        <th>nome</th>
+                        <th>endereço</th>
+                        <th>telefone</th>
+                        <th>ações</th>
+                    </tr>
+                    <?php foreach(new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v){
+                        echo "<tr>";
+                        echo "<td>".$v["nome"]."</td>";
+                        echo "<td>".$v["endereco"]."</td>";
+                        echo "<td>".$v["telefone"]."</td>";
+                      echo "<td>
+                      <a href='../paginas/edit_usuario.php?id=$v[id]' title='Editar'><img src='../assets/img/edt.png'\></a>
+                        <a href='../model/clientesdelete.php?id=$v[id]' title='Deletar'><img src='../assets/img/del.png'\></a>
+                        </td>";
+                        echo "<tr>";
+                    }
+                    ?>
+                </table>
+            </div>
+          </div>
+  </div> 
 </body>
 </html>
